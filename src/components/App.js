0ec63b149ccea0c272920f5477b35c1ed500c629
Prashant Mishra <103806839+pr-a-s-h-a-n-t@users.noreply.github.com>
2022-11-10 17:46:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
-function App() {
+  
+import "./styles.css";
+
+export default function App() {
+  let para =   `Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy`
+  
+  const[display, setdisplay] = React.useState(false)
+  function renderComponent(){
+    return( setdisplay(true)
+    )
+
+  }
+
+  let show;
+  if(display){
+    show = para;
+  }
+  else{
+    show = null;
+  }
   return (
-    <div id="main">
-      // Do not alter the main div
+    <div className="App">
+      <button id="click" onClick={ renderComponent }>Submit</button>
+      <p id="para"  >{ show }</p>
     </div>
   );
 }
 
-
-export default App;
