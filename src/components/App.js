bb6af717f300c25088376335e84482638ba0aa5b
Prashant Mishra <prashantmishra111@outlook.com>
2022-11-28 10:16:09 +0530

    !

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,24 @@
-import React, {Component, useState} from "react";
+import React, {  useState} from "react";
 import "./../styles/App.css";
 
   
-import "./styles.css";
+ 
 
 export default function App() {
-  let para =   `Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy`
+     
   
-  const[display, setdisplay] = React.useState(false)
-  function renderComponent(){
-    return( setdisplay(true)
-    )
+  const[display, setdisplay] =  useState(false)
+   
 
-  }
+   
 
-  let show;
-  if(display){
-    show = para;
-  }
-  else{
-    show = null;
-  }
+   
   return (
     <div className="App">
-      <button id="click" onClick={ renderComponent }>Submit</button>
-      <p id="para"  >{ show }</p>
+      <button id="click" onClick={ () => {
+        setdisplay(true);
+      } }>Add</button>
+      {display && <p id="para"  >Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy </p>}
     </div>
   );
 }
